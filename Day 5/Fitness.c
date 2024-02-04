8f26065411a692066879e7929480755f27a3f5bd
Ishit-Singh05 <154051822+Ishit-Singh05@users.noreply.github.com>
2024-02-04 20:00:16 +0530

    Created Fitness.c

---
+++ Day 5/Fitness.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    int i,a;
+    for(i=0;i<t;++i){
+       scanf("%d",&a);
+       int dis=10*a;
+       printf("%d\n",dis);
+    }
+}
