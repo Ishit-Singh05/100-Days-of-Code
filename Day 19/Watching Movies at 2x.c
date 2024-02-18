f260889a2843ec9bbadf8087707aa38fe598e0d6
Ishit-Singh05 <154051822+Ishit-Singh05@users.noreply.github.com>
2024-02-18 18:39:50 +0530

    Created Watching Movies at 2x.c

---
+++ Day 19/Watching Movies at 2x.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+
+	    int x,y;
+	    scanf("%d %d",&x,&y);
+	    int time=y/2;
+	    int remain=x-y;
+	    int total=remain+time;
+	    printf("%d\n",total);
+}
+
