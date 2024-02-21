e1a5fa0fa7280be66da3ee538abc1e058e6704db
Ishit-Singh05 <154051822+Ishit-Singh05@users.noreply.github.com>
2024-02-21 18:44:14 +0530

    Created Finding Square Roots.c

---
+++ Day 22/Finding Square Roots.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	for(int i=0;i<t;++i){
+	    int n,a;
+	    scanf("%d",&n);
+	    a=sqrt(n);
+	    printf("%d\n",a);
+	}
+
+}
