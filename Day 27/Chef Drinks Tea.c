33e93ea8d7890fbf00f62b8f000fe0defc997251
Ishit-Singh05 <154051822+Ishit-Singh05@users.noreply.github.com>
2024-02-26 18:41:16 +0530

    Created Chef Drinks Tea.c

---
+++ Day 27/Chef Drinks Tea.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	for(int i=0;i<t;++i){
+	    int a,b,c;
+	    scanf("%d %d %d",&a,&b,&c);
+	    
+	    if(a<b || a==b)
+        {
+            printf("%d \n",c);
+        }
+        
+        else if(a%b==0)
+        {
+            printf("%d \n",((a/b)*c));
+        }
+        else
+        {
+            printf("%d \n",((a/b)+1)*c);
+        }
+	}
+
+}
+
