55cc07d567714280ee2a961166122aa8c5a80d81
Ishit-Singh05 <154051822+Ishit-Singh05@users.noreply.github.com>
2024-02-27 18:06:37 +0530

    Created Maximise the Tastiness.c

---
+++ Day 28/Maximise the Tastiness.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	for(int i=0;i<t;++i){
+	    int a,b,c,d,x,y;
+	    scanf("%d %d %d %d",&a,&b,&c,&d);
+	    if(a>b){
+	        x=a;
+	    }
+	    else{
+	        x=b;
+	    }
+	    if(c>d){
+	        y=c;
+	    }
+	    else{
+	        y=d;
+	    }
+	    printf("%d\n",x+y);
+	}
+
+}
+
