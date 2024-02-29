70e96482b5327fc7d26952434345424aa73ea0c7
Ishit-Singh05 <154051822+Ishit-Singh05@users.noreply.github.com>
2024-02-29 18:16:15 +0530

    Created Bath in Winters.c

---
+++ Day 30/Bath in Winters.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	for(int i=0;i<t;i++){
+	    int x,y;
+	    scanf("%d %d",&x,&y);
+	    int c=2*y;
+	    printf("%d\n",x/c);
+	}
+
+}
+
