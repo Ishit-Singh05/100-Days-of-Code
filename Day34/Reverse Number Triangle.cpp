f8b3f305473eb148b2182a6a9845ec703412b1f8
Ishit-Singh05 <154051822+Ishit-Singh05@users.noreply.github.com>
2024-03-04 18:19:06 +0530

    Created Reverse Number Triangle.cpp

--- Day34/Reverse Number Triangle.cpp
+++ Day34/Reverse Number Triangle.cpp
@@ -1,7 +1,7 @@
 void nNumberTriangle(int n) {
     // Write your code here.
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i+1;j++){
+        for(int j=1;j<=n-i+1;j++)
             cout<<j<<" ";
         }
         cout<<endl;
