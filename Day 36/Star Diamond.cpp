491ba67f3bca5d075694c9037e06e2401bafe14e
Ishit-Singh05 <154051822+Ishit-Singh05@users.noreply.github.com>
2024-03-06 18:27:50 +0530

    Created Star Diamond.cpp

---
+++ Day 36/Star Diamond.cpp
@@ -0,0 +1,35 @@
+void nStarDiamond(int n) {
+    // Write your code here.
+    for(int i=0;i<n;i++){
+        //spaces
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        //stars
+        for(int j=0;j<2*i+1;j++){
+            cout<<"*";
+        }
+        //spaces
+        for (int j = 0; j < n - i - 1; j++) {
+            cout << " ";
+        }
+        cout << endl;
+    }
+    for(int i=0;i<n;i++){
+        //spaces
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        //stars
+        for(int j=0;j<2*n-(2*i+1);j++){
+            cout<<"*";
+        }
+        //spaces
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+
+        cout<<endl;
+    }
+       
+}
