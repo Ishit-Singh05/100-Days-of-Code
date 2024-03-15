e9f2950adaad2fd98c554adadf9bf5a86195b211
Ishit-Singh05 <154051822+Ishit-Singh05@users.noreply.github.com>
2024-03-15 21:15:24 +0530

    Created Alpha-Triangle.cpp

---
+++ Day 45/Alpha-Triangle.cpp
@@ -0,0 +1,11 @@
+void alphaTriangle(int n) {
+    // Write your code here.
+    for(int i=0;i<n;i++){
+        // char ch='A'+n-1;
+        for(char ch='A'+n-1;ch>'A'+(n-i-2);ch--){
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+   
+}
