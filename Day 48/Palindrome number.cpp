6de51bc8a3672876832780a26c9824af41746f1d
Ishit-Singh05 <154051822+Ishit-Singh05@users.noreply.github.com>
2024-03-18 11:30:37 +0530

    Created  Palindrome number.cpp

---
+++ Day 48/Palindrome number.cpp
@@ -0,0 +1,13 @@
+bool palindrome(int n)
+{
+    // Write your code here
+    int reverse=0;
+    int original =n;
+    while(n>0){
+        int last=n%10;
+        reverse=(reverse*10)+last;
+        n=n/10;
+    }
+    if(original==reverse) return true;
+    else return false;
+}
