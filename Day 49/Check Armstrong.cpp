23276d62360d86f86cf39d4ea40c56ae94c2c509
Ishit-Singh05 <154051822+Ishit-Singh05@users.noreply.github.com>
2024-03-19 18:20:39 +0530

    Created Check Armstrong.cpp

---
+++ Day 49/Check Armstrong.cpp
@@ -0,0 +1,19 @@
+#include <math.h>
+bool checkArmstrong(int n){
+	//Write your code here
+	int count=0;
+	int original=n;
+	int temp=n;
+	while(temp>0){
+		count++;
+		temp=temp/10;
+	}
+	int sum=0;
+	while(n>0){
+		int last=n%10;
+		sum+=pow(last,count);
+		n=n/10;
+	}
+	if(sum==original) return true;
+	else return false;
+}
