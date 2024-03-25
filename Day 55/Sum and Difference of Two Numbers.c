ba7f3b02435b317cd7797800e1eb1bb1192b6881
Ishit-Singh05 <154051822+Ishit-Singh05@users.noreply.github.com>
2024-03-25 16:25:43 +0530

    Created Sum and Difference of Two Numbers.c

---
+++ Day 55/Sum and Difference of Two Numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b;
+    float c,d;
+    scanf("%d %d",&a,&b);
+    scanf("%f %f",&c,&d);
+    
+    int sum=a+b,dif=a-b;;
+   float sumf=c+d,difs=c-d;
+   
+   printf("%d %d\n",sum,dif);
+   printf("%.1f %.1f\n",sumf,difs);
+     
+    
+    return 0;
+}
